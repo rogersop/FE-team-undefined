501cd07367bd784d1e40c567e0149afb430fdfea
rogersop <paulerogerson@gmail.com>
2018-01-22 11:53:19 +0000

    create-react-app, update read-me and app name, removing unneccessary files

---
+++ do-more/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">DoMore</h1>
+        </header>
+        <p className="App-intro">
+          Welcome to DoMore.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
