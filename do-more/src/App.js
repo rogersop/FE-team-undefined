b376d9e206624394d424161e7d3ce09820ee5270
tomathon <tomathonmiller@gmail.com>
2018-01-22 14:56:39 +0000

    create InfoWidget and all component boxes within it

--- do-more/src/App.js
+++ do-more/src/App.js
@@ -3,15 +3,18 @@ import NewsWidget from './components/NewsWidget';
 import EmailWidget from './components/EmailWidget';
 import CalendarWidget from './components/CalendarWidget';
 import TodoWidget from './components/TodoWidget';
-import WeatherWidget from './components/WeatherWidget'
+import InfoWidget from './components/InfoWidget';
 
 class App extends Component {
   
   render() {
       const style = {
-        "background": "url('https://images.unsplash.com/photo-1489844097929-c8d5b91c456e')",
-        "background-size":"cover"
-      }
+        "background-image": "url('https://images.unsplash.com/photo-1489844097929-c8d5b91c456e')",
+        "bacground-repeat": "no-repeat",
+        "background-position": "center",
+        "background-size":"cover",
+    }
+
     return (
       <div className="App" style={style}>
         <header className="App-header">
@@ -20,11 +23,11 @@ class App extends Component {
         <p className="App-intro">
           Welcome to DoMore.
         </p>
+        <InfoWidget />
         <NewsWidget />
         <EmailWidget />
         <CalendarWidget />
         <TodoWidget />
-        <WeatherWidget />
       </div>
     );
   }
--- do-more/src/components/WeatherBox.js
+++ do-more/src/components/WeatherBox.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 
-class WeatherWidget extends Component {
+class WeatherBox extends Component {
 
   render () {
     return (
-      <div className="weather-widget">
         <div className="weather-box">
           <img style={{"height": "40px"}}src="http://cdn.mysitemyway.com/icons-watermarks/simple-black/raphael/raphael_rain-cloud/raphael_rain-cloud_simple-black_512x512.png" />
           <h2>7°C</h2>
           <h3>Rain</h3>
           <p>Manchester grey all day long</p>
         </div>
-      </div>
     )
   }
 }
 
-export default WeatherWidget;
+export default WeatherBox;
