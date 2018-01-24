55a00792cda423e2318bafab6da19722bbcb9746
rogersop <paulerogerson@gmail.com>
2018-01-24 15:39:43 +0000

    implement functionality to auto fetch emails on sign-in / page render, and auto clear on sign-out

--- do-more/src/index.js
+++ do-more/src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App fetchFiveEmails={window.fetchFiveEmails} /> , document.getElementById('root'));
+ReactDOM.render(
+  <App 
+    fetchFiveEmails={window.fetchFiveEmails} 
+    authClick={window.authClick} 
+  /> , document.getElementById('root'));
