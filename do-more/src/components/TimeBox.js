d5e1a0425dc9a9d8ec68455fe56f9365dcc6f434
rogersop <paulerogerson@gmail.com>
2018-01-25 16:13:27 +0000

    fix invalid date format in sidebar and change order of date greeting weather

--- do-more/src/components/TimeBox.js
+++ do-more/src/components/TimeBox.js
@@ -5,54 +5,28 @@ import moment from 'moment';
 class TimeBox extends Component {
 
   state = {
-    curTime: new Date().toLocaleString(),
+    curTime: '',
   }
 
   componentDidMount() {
-
     setInterval(() => {
       this.setState({
-        curTime: new Date().toLocaleString(),
+        curTime: moment(),
       })
     }, 1000)
 
   }
-
 
   render() {
     let date = new Date();
 
     return (
       <div className="time-box">
-        <div>{moment(this.state.curTime).format('h:mm')}</div>
         <span>{moment(date).format('MMMM Do YYYY')}</span>
-
-
+        <div>{moment(this.state.curTime).format('HH:mm')}</div>
       </div>
     )
   }
 }
 
 export default TimeBox;
-
-//var Timer = React.createClass({
-//   getInitialState: function() {
-//     return {secondsElapsed: 0};
-//   },
-//   tick: function() {
-//     this.setState({secondsElapsed: this.state.secondsElapsed + 1});
-//   },
-//   componentDidMount: function() {
-//     this.interval = setInterval(this.tick, 1000);
-//   },
-//   componentWillUnmount: function() {
-//     clearInterval(this.interval);
-//   },
-//   render: function() {
-//     return (
-//       <div>Seconds Elapsed: {this.state.secondsElapsed}</div>
-//     );
-//   }
-// });
-
-// ReactDOM.render(<Timer />, mountNode);
