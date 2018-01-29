c33fa726a5fe299924b66d17137a38ed7fbb7f7a
LukeFenn <lgfenn@gmail.com>
2018-01-29 12:34:45 +0000

    Todo list v2 (#14)
    * todo list refactor for marking as complete and filtering on all, done and todo
    
    * implement localStorate for todo list

--- do-more/src/components/TodoWidget.js
+++ do-more/src/components/TodoWidget.js
@@ -1,37 +1,24 @@
 import React, { Component } from 'react';
-
+import TodoItem from './TodoItem'
 class TodoWidget extends Component {
 
   state = {
     todoItems: [],
-    inputText: ''
+    inputText: '',
+    selectedFilter: "All"
   }
 
 
   componentDidMount = () => {
-    // need to add functionality to GET todo items from database for the user
-    this.setState({
-      todoItems: [
-        {
-          text: "this is my first todo item",
-          complete: false
-        },
-        {
-          text: "this is my first todo item again",
-          complete: false
-        },
-        {
-          text: "this is my first todo item again again",
-          complete: false
-        },
-        {
-          text: "this is my first todo item again again again",
-          complete: true
-        }
-      ]
-    }) 
-  }
-
+      const todoState = JSON.parse(localStorage.getItem("todoState"));
+      if(localStorage.todoState) {
+        this.setState({
+        todoItems: todoState.todoItems,
+        inputText: todoState.inputText,
+        selectedFilter: todoState.selectedFilter
+      })
+    } 
+  } 
   updateInputText = (event) => {
     this.setState({
       inputText: event.target.value
@@ -39,22 +26,67 @@ class TodoWidget extends Component {
   }
 
   addTodoItem = () => {
-    console.log(this.state.todoItems, "state before addtodo setstate")
     const newTodo = {text: this.state.inputText, complete: false}
     if (this.state.inputText === '') return;
     this.setState({
-      todoItems: [...this.state.todoItems, newTodo]
-    })
-    // need to add functionality to PUT new todo item in database for the user
+      todoItems: [...this.state.todoItems, newTodo],
+      inputText: ""
+    }, res => {localStorage.setItem("todoState", JSON.stringify(this.state))})
   }
 
-  // filterTodoList () => {
 
-  // }
+
+  toggleTodo = (event) => {
+    const index = +event.target.dataset.index
+
+    const todo = this.state.todoItems[index]
+
+    const newTodo = Object.assign({}, todo, {
+      complete: !todo.complete
+    })
+
+    const newTodos = [
+      ...this.state.todoItems.slice(0, index),
+      newTodo,
+      ...this.state.todoItems.slice(index + 1)
+    ]
+
+    this.setState({
+      todoItems: newTodos
+    }, res => {localStorage.setItem("todoState", JSON.stringify(this.state))})
+  }
+
+
+  filterTodos = (todos, filter) => {
+    return todos.filter(todo => {
+      if(filter === "Done") return todo.complete
+      else if( filter === "Todo") return !todo.complete
+      else return true;
+    })
+  }
+
+
+  setFilterAll = () => {
+    this.setState({
+      selectedFilter: "All"
+    })
+  }
+
+  setFilterDone = () => {
+    this.setState({
+      selectedFilter: "Done"
+    })
+  }
+
+  setFilterTodo = () => {
+    this.setState({
+      selectedFilter: "Todo"
+    })
+  }
 
 
   render() {
-    const {todoItems, inputText} = this.state;
+    const {todoItems, inputText, selectedFilter} = this.state;
 
     return (
       <div className="todo-widget">
@@ -62,18 +94,23 @@ class TodoWidget extends Component {
           <h3>To-Do:</h3>
         </div>
         <div className="todo-list">
-        {todoItems.map((item, i) => {
-          return <div key={i} className="todo-item">
-              <p>{item.text}</p>
-            </div>
+        {this.filterTodos(todoItems, selectedFilter).map((item, i) => {
+         return <TodoItem
+           key={i} 
+           index={i}
+           complete={item.complete}
+           toggleTodo={this.toggleTodo}
+           >
+           {item.text}
+           </TodoItem>
         })}
         </div>
 
         <div className="todo-footer">
           <div className="todo-filter">
-            <span>All</span>{" | "}
-            <span onClick={this.filterTodoList}>Done</span>{" | "}
-            <span>To-Do</span>
+            <span className="filter-option" onClick={this.setFilterAll} value="All">All</span>{" | "}
+            <span className="filter-option" onClick={this.setFilterDone} value="Done">Done</span>{" | "}
+            <span className="filter-option" onClick={this.setFilterTodo} value="To-Do">To-Do</span>
           </div>
 
           <div className="todo-input">
@@ -87,3 +124,6 @@ class TodoWidget extends Component {
 }
 
 export default TodoWidget;
+
+
+
