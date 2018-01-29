0fff57033ce7bf34a821bb58c7b6fe7e5a9d2ff9
Paul Rogerson <paulerogerson@gmail.com>
2018-01-29 17:08:51 +0000

    seed initial firebase db and add functions to update usecounts of widgets and background likes (#17)

--- do-more/src/database/index.js
+++ do-more/src/database/index.js
@@ -1,37 +1,56 @@
 const firebase = require('firebase');
 
 let config = {
-  apiKey: "AIzaSyC021C6kMFD7rgBY3Ibv6sLRyplwNunZW8",
-  authDomain: "twitter-app-3b663.firebaseapp.com",
-  databaseURL: "https://twitter-app-3b663.firebaseio.com",
-  projectId: "twitter-app-3b663",
-  storageBucket: "twitter-app-3b663.appspot.com",
-  messagingSenderId: "63643362222"
+  apiKey: "AIzaSyDMqDVxZq28Oi44ZNm84POsHu7nvkttFX0",
+  authDomain: "team-undefined-productivity.firebaseapp.com",
+  databaseURL: "https://team-undefined-productivity.firebaseio.com",
+  projectId: "team-undefined-productivity",
+  storageBucket: "team-undefined-productivity.appspot.com",
+  messagingSenderId: "716837513754"
 };
 firebase.initializeApp(config);
 
 const db = firebase.database();
 
-function updateUseCount (widget, increase) {
+const widgets = ['twitterWidget', 'todoWidget', 'calendarWidget', 'emailWidget', 'newsWidget']
+
+function createWidgetObject (widget) {
+  db.ref(`widgets/${widget}/`).set({
+    useCount: 0
+  })
+}
+
+const increaseUseCount = (widget) => {
   db.ref(`widgets/${widget}/useCount`).transaction(currentCount => {
     return currentCount + 1;
   });
 }
 
-function postBackgroundImages (imageSources) {
-  imageSources.forEach(source => {
-    db.ref('backgrounds').push({
-      source,
-      useCount : 0
+const decreaseUseCount = (widget) => {
+  db.ref(`widgets/${widget}/useCount`).transaction(currentCount => {
+    return currentCount - 1;
+  });
+}
+
+function getWidgetsInfo () {
+  return db.ref('widgets').once('value')
+    .then(wgtState => {
+      let widgets = wgtState.val();
+      widgets = Object.keys(widgets).map(key => {
+        return Object.assign({}, widgets[key], {key})
+      })
+      return widgets;
     })
+}
+
+function seedImageInfo (image) {
+  db.ref(`backgrounds/`).push({
+    url: image,
+    likeCount: 0
   })
 }
 
-const sources = ['mountain', 'morning rain', 'mid-afternoon sleet', 'volcano'];
-
-// postBackgroundImages(sources);
-
-const getImages = () => {
+const getAllImagesInfo = () => {
   return db.ref('backgrounds').once('value')
     .then(bgState => {
       let backgrounds = bgState.val();
@@ -42,4 +61,46 @@ const getImages = () => {
     })
 }
 
-getImages().then(console.log);
+const getImageInfo = (imageKey) => {
+  return db.ref(`backgrounds/${imageKey}`).once('value')
+    .then(imageInfo => {
+     return imageInfo.val();
+    })
+}
+
+
+const increaseLikeCount = (imageKey) => {
+  db.ref(`backgrounds/${imageKey}/likeCount`).transaction(currentCount => {
+    return currentCount + 1;
+  });
+}
+
+const decreaseLikeCount = (imageKey) => {
+  db.ref(`backgrounds/${imageKey}/likeCount`).transaction(currentCount => {
+    return currentCount - 1;
+  });
+}
+
+const getRandomImage = () => {
+  getAllImagesInfo().then(imagesArr => {
+    return imagesArr[Math.floor(Math.random() * (imagesArr.length - 1))]
+  })
+}
+
+getRandomImage();
+
+// const imageUrls = ['http://res.cloudinary.com/teamundefined/image/upload/q_100/v1516794822/2cqorg_ml5050_hm7vad.jpg', 
+// 'http://res.cloudinary.com/teamundefined/image/upload/q_100/v1516794822/saint-coloman-3092260_1920_yz2ojw.jpg', 
+// 'http://res.cloudinary.com/teamundefined/image/upload/v1516794822/peacock-3080897_1920_elwjrf.jpg', 
+// 'http://res.cloudinary.com/teamundefined/image/upload/v1516794821/tlnvoe_jakstashoe_ftmmwt.jpg',
+// 'http://res.cloudinary.com/teamundefined/image/upload/v1516794821/l52a2f_kash_b0zezm.jpg',
+// 'http://res.cloudinary.com/teamundefined/image/upload/v1516794820/DSCF2114_lu8ov8.jpg',
+// 'http://res.cloudinary.com/teamundefined/image/upload/v1516794820/h6rcqc_bardhi_ylgany.jpg',
+// 'http://res.cloudinary.com/teamundefined/image/upload/v1516794820/nature-3082832_1920_ogo0y1.jpg',
+// 'http://res.cloudinary.com/teamundefined/image/upload/v1516794819/lf23e6_kash_ybrkwe.jpg',
+// 'http://res.cloudinary.com/teamundefined/image/upload/v1516794818/pexels-photo-454880_yehbuz.jpg',
+// 'http://res.cloudinary.com/teamundefined/image/upload/v1516794818/7owwvl_alex2411_bpqcvd.jpg',
+// 'http://res.cloudinary.com/teamundefined/image/upload/v1516794818/mt-fuji-sea-of-clouds-sunrise-46253_oahcqf.jpg',
+// 'http://res.cloudinary.com/teamundefined/image/upload/v1516794818/w2ju24_bardhi_axbl6f.jpg',
+// 'http://res.cloudinary.com/teamundefined/image/upload/v1516794817/q2c1iq_anthonybhora_egpgpo.jpg',
+// 'http://res.cloudinary.com/teamundefined/image/upload/v1516794817/sunset-3092048_1920_j6l3go.jpg']
